2c42aaa406c5ad1cd0ed3f7ce6917f79f7a5251a
FeliOdras <ariane.feldmann@odras.de>
2019-04-09 11:58:09 +0200

    Created component with function for clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Header from './Header'
+import Clock from './Clock'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-
+        <main>
+          <Clock />
+        </main>
       </div>
     );
   }
