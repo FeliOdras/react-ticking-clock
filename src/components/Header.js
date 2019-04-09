b3d37b483ab82a89ca4100a4140b5aa6dda8c5cc
FeliOdras <ariane.feldmann@odras.de>
2019-04-09 11:49:07 +0200

    Created Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const _Header = () => {
+    return (
+        <header className="App-header">
+            <div className="hotpink-logo">#hotpink</div>
+            <h1>The clock is ticking</h1>
+        </header>
+    )
+}
+
+export default _Header
